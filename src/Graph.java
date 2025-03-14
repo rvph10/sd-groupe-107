d054f5cbdf527fa7d402ec0889bec010917586e4
rvph10 <rgenu10@gmail.com>
2025-03-14 12:08:16 +0100

    init of artists

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+
+
 public class Graph {
-    private String fichierArtistes;
-    private String fichierMentions;
+
+    private Map<String, Artist> artistes;
 
     public Graph(String fichierArtistes, String fichierMentions) {
-        this.fichierArtistes = fichierArtistes;
-        this.fichierMentions = fichierMentions;
+        initArtistes(fichierArtistes);
+    }
+
+    private void initArtistes(String fichierArtistes){
+        String linkfile = "data/" + fichierArtistes;
+        try (BufferedReader br = new BufferedReader(new FileReader(linkfile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                HashSet<String> categories = new HashSet<String>(Arrays.asList(parts[2].split(";")));
+                Artist artist = new Artist(Integer.parseInt(parts[0]), parts[1], categories);
+                artistes.put(parts[1], artist);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void trouverCheminLePlusCourt(String artiste1, String artiste2) {
