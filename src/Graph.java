f24e3112f50a65af0720ff9c2285d8cefef7c64a
rvph10 <rgenu10@gmail.com>
2025-03-14 11:32:56 +0100

    added graph

---
+++ src/Graph.java
@@ -0,0 +1,21 @@
+public class Graph {
+    private String fichierArtistes;
+    private String fichierMentions;
+
+    public Graph(String fichierArtistes, String fichierMentions) {
+        this.fichierArtistes = fichierArtistes;
+        this.fichierMentions = fichierMentions;
+    }
+
+    
+
+    public void trouverCheminLePlusCourt(String artiste1, String artiste2) {
+        // TODO
+        return;
+    }
+
+    public void trouverCheminMaxMentions(String artiste1, String artiste2) {
+        // TODO
+        return;
+    }
+}
